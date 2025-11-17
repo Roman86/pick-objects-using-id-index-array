0168b7d821493cb30abd9e88cba619da2c4ea32a
Roman Kozodoi <romych.sync@gmail.com>
2025-11-17 13:51:27 +0100

    fix: license changed from ISC to MIT, bundler changed to tsup, dev dependencies update

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,17 @@
 // will return matching objects in given order. Notice, that ids and objects may not match
-export function pickObjects<T extends Record<string, any>, ID>(
+export function pickObjects<T extends Record<string, V>, ID, V>(
   ids: ID[],
   objects: T[],
   idField: keyof T | ((item: T) => ID),
 ): T[] {
   const idMap = new Map<ID, T>(
     objects.map((o) => [
-      typeof idField === 'function' ? idField(o) : o[idField],
+      typeof idField === 'function' ? idField(o) : o[idField] as ID,
       o,
-    ]),
+    ] as const),
   );
 
-  return ids.map((id) => idMap.get(id)).filter((o) => typeof o !== 'undefined') as T[];
+  return ids
+    .map((id) => idMap.get(id))
+    .filter((o) => typeof o !== 'undefined') as T[];
 }
--- src/pickObject.test.ts
+++ src/pickObject.test.ts
@@ -2,90 +2,22 @@ import { pickObjects } from './index';
 
 test('pickObjects', () => {
   expect(
-    pickObjects(
-      [
-        1,
-        2,
-        3,
-      ],
-      [
-        { id: 1 },
-        { id: 2 },
-        { id: 3 },
-      ],
-      'id',
-    ),
-  ).toStrictEqual([
-    { id: 1 },
-    { id: 2 },
-    { id: 3 },
-  ]);
+    pickObjects([1, 2, 3], [{ id: 1 }, { id: 2 }, { id: 3 }], 'id'),
+  ).toStrictEqual([{ id: 1 }, { id: 2 }, { id: 3 }]);
 
   expect(
-    pickObjects(
-      [
-        3,
-        2,
-        1,
-      ],
-      [
-        { id: 1 },
-        { id: 2 },
-        { id: 3 },
-      ],
-      'id',
-    ),
-  ).toStrictEqual([
-    { id: 3 },
-    { id: 2 },
-    { id: 1 },
-  ]);
+    pickObjects([3, 2, 1], [{ id: 1 }, { id: 2 }, { id: 3 }], 'id'),
+  ).toStrictEqual([{ id: 3 }, { id: 2 }, { id: 1 }]);
 
   expect(
-    pickObjects(
-      [
-        1,
-      ],
-      [
-        { id: 1 },
-        { id: 2 },
-        { id: 3 },
-      ],
-      'id',
-    ),
-  ).toStrictEqual([
-    { id: 1 },
-  ]);
+    pickObjects([1], [{ id: 1 }, { id: 2 }, { id: 3 }], 'id'),
+  ).toStrictEqual([{ id: 1 }]);
 
   expect(
-    pickObjects(
-      [
-        5,
-        2,
-      ],
-      [
-        { id: 1 },
-        { id: 2 },
-        { id: 3 },
-      ],
-      'id',
-    ),
-  ).toStrictEqual([
-    { id: 2 },
-  ]);
+    pickObjects([5, 2], [{ id: 1 }, { id: 2 }, { id: 3 }], 'id'),
+  ).toStrictEqual([{ id: 2 }]);
 
   expect(
-    pickObjects(
-      [
-        5,
-      ],
-      [
-        { id: 1 },
-        { id: 2 },
-        { id: 3 },
-      ],
-      'id',
-    ),
-  ).toStrictEqual([
-  ]);
+    pickObjects([5], [{ id: 1 }, { id: 2 }, { id: 3 }], 'id'),
+  ).toStrictEqual([]);
 });
